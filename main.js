ff676155057ca67e5a2c192378198a754bed6a6f
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2024-10-11 22:44:35 +0530

    intial stage

---
+++ main.js
@@ -0,0 +1,15 @@
+let mode = document.querySelector("#bar1");
+let x = 1;
+mode.addEventListener("click", () => {
+    event.preventDefault();
+    let change = document.querySelector('.welcome');
+    if(x<3){
+        
+    }
+});
+
+
+
+
+
+
